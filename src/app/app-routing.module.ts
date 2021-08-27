9ce9882d03d61fdbe99b9211b51be8f8f5d62d09
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-27 17:17:03 +0300

    add routes in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //to add later
+  //to add later
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
