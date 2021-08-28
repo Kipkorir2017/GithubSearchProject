d6668902e7fc6fbdd70aaef06c5df0a2f2d33395
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-28 08:58:43 +0300

    add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  //to add later
-  //to add later
+ 
+  { path: 'HOME', component: LandingComponent},
+  { path: 'PROFILE', component: ProfileComponent },
+  { path: '', redirectTo:"/PROFILE", pathMatch:"full"},
   { path:'**', component:NotFoundComponent},
 ];
 
