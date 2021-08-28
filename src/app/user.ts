1c19f0603f822a1a9f55b0c12788bc95749464f7
Kipkorir2017 <pkobenj41@gmail.com>
2021-08-28 09:35:40 +0300

    add changes to the profile component

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(public avatar_url:string,
+        public followers:number,
+        public following:number,
+        public repos_url:string,
+        public  bio:string,
+        public location:string,
+        public created_at:Date
+        ){
+
+    }
 }
